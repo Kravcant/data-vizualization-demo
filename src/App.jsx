fecf01cb03343f06818f086a65c2fbde3bafdd91
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2026-06-07 20:29:53 -0700

    Configured all charts to render with headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import Step1 from './Step1.jsx';
-import Step3 from './Step3.jsx';
 import Consoles from './Consoles.jsx';
+import PieCompanySales from './components/PieChart.jsx';
+import LineYearSales from './components/LineChart.jsx';
+import MulitLineCompanyTrends from './components/AreaChart.jsx'
+import BarTypeSales from './components/HorizontalBarChart.jsx'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <Step1 />
-      <Step3 />
-      <Consoles />
-    </>
-  )
+    <div className="page">
+      <h1>Video Game Console Sales</h1>
+      <p style={{ textAlign: "center", opacity: 0.7 }}>
+        A quick visualization of console sales using Recharts
+      </p>
+
+      <div className="chart">
+        <h2>Total Consoles Sales</h2>
+        <Consoles />
+      </div>
+
+      <div className="chart">
+        <h2>Sales by Company</h2>
+        <PieCompanySales />
+      </div>
+
+      <div className="chart">
+        <h2>Sales by Year</h2>
+        <LineYearSales />
+      </div>
+
+      <div className="chart">
+        <h2>Nintendo vs Sony vs Microsoft</h2>
+        <MulitLineCompanyTrends />
+      </div>
+
+      <div className="chart">
+        <h2>Sales by Console Type</h2>
+        <BarTypeSales />
+      </div>
+    </div>
+  );
 }
 
 export default App
